727836fff52f3fd2346843ee0a34654eb836edcf
Afara Seun <seunvigo@gmail.com>
2020-10-14 13:21:00 +0100

    about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="port-home padder">
+    <section class="section-hero about-hero">
+      <h2 class="hero-intro about-intro">
+        Afara Seun Abiola <br /><span class="intro-big">Coding warrior.</span>
+        <br />
+        <span class="intro-sm">
+          <span style="color: #BABBC2;"
+            >A full stack software engineer with more than 5 years of coding
+            experience.</span
+          >
+          <br />
+          Responsible for turning just ideas into real-world products for
+          clients such as Dangote, Vigowork, Triangle Nigeria, Dremo, PFI
+          Capital and many others. And when Seun is not coding, he's reading
+          books from great authors, e.g "Thinking, fast and slow", "Contagious,
+          why things catch on", "The psychology of selling" and many more great
+          books.</span
+        >
+      </h2>
+      <img
+        class="memoji-about"
+        src="../../public/uploads/memoji_4.webp"
+        alt=""
+      />
+    </section>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
